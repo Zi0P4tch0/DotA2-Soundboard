97ea3420b30aeaa9fb53300cf3bb5ac4a55fbad6
Matteo Pacini <ispeakprogramming@gmail.com>
2013-08-18 11:40:28 +0100

    Added GPL license

--- DotA2Soundboard/D2SBMasterViewController.h
+++ DotA2Soundboard/D2SBMasterViewController.h
@@ -1,10 +1,21 @@
-//
-//  D2SBMasterViewController.h
-//  DotA2Soundboard
-//
-//  Created by Matteo Pacini on 18/07/13.
-//  Copyright (c) 2013 Matteo Pacini. All rights reserved.
-//
+///////////////////////////////////////////////////////////////////////////////
+//                                                                           //
+//  This file is part of DotA2Soundboard.                                    //
+//                                                                           //
+//  DotA2Soundboard is free software: you can redistribute it and/or modify  //
+//  it under the terms of the GNU General Public License as published by     //
+//  the Free Software Foundation, either version 3 of the License, or        //
+//  any later version.                                                       //
+//                                                                           //
+//  DotA2Soundboard is distributed in the hope that it will be useful,       //
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of           //
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            //
+//  GNU General Public License for more details.                             //
+//                                                                           //
+//  You should have received a copy of the GNU General Public License        //
+//  along with DotA2Soundboard.  If not, see <http://www.gnu.org/licenses/>. //
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
 
 #import <UIKit/UIKit.h>
 
